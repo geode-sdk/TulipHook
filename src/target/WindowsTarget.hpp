8d653f92c17edbc787e6e9dde8eda80596e63545
altalk23 <45172705+altalk23@users.noreply.github.com>
2024-04-21 13:55:54 +0300

    fix the forgoten artifact

--- src/target/WindowsTarget.hpp
+++ src/target/WindowsTarget.hpp
@@ -18,7 +18,6 @@ namespace tulip::hook {
 		Result<uint32_t> getProtection(void* address) override;
 		Result<> protectMemory(void* address, size_t size, uint32_t protection) override;
 		Result<> rawWriteMemory(void* destination, void const* source, size_t size) override;
-		Result<> writeMemory(void* destination, void const* source, size_t size) override;
 		uint32_t getWritableProtection() override;
 
 		std::unique_ptr<HandlerGenerator> getHandlerGenerator(
