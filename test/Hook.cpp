617001d7184193e09cb4410287829b1cc081bc8c
altalk23 <45172705+altalk23@users.noreply.github.com>
2024-06-02 13:10:20 +0300

    test4

--- test/Hook.cpp
+++ test/Hook.cpp
@@ -89,14 +89,14 @@ TEST(HookTest, NoHandler) {
 
 TEST(HookTest, NoHooks) {
 	HandlerHandle handlerHandle;
-	makeHandler<1>(handlerHandle);
+	// makeHandler<1>(handlerHandle);
 	EXPECT_EQ(callFunction<1>(), 1);
 }
 
 TEST(HookTest, MakeWrapper) {
 	FunctionPtrType unwrapped;
 	makeWrapper<2>(unwrapped);
-	// EXPECT_EQ(unwrapped(1, 2, 3, 4, 5, 6, 7, 8, 9, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f), 1);
+	EXPECT_EQ(unwrapped(1, 2, 3, 4, 5, 6, 7, 8, 9, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f), 1);
 }
 
 // hook -> function
