b96b637ae736c16f928ca1f4cd373bca09b54930
altalk23 <45172705+altalk23@users.noreply.github.com>
2024-06-02 13:25:19 +0300

    early test arm

--- src/generator/ArmV8Generator.cpp
+++ src/generator/ArmV8Generator.cpp
@@ -49,7 +49,7 @@ std::vector<uint8_t> ArmV8HandlerGenerator::handlerBytes(uint64_t address) {
 	a.pop({X0, X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12, X13, X14, X15});
 
 	// call the func
-	a.blr(X30);
+	// a.blr(X30);
 
 	// preserve the return values
 	a.push({X0, X1, X2, X3, X4, X5, X6, X7});
