f3e66da046f6826ac3033f45e1e1505366a927f1
altalk23 <45172705+altalk23@users.noreply.github.com>
2025-06-13 12:56:31 +0300

    random

--- src/disassembler/ArmV8Disassembler.cpp
+++ src/disassembler/ArmV8Disassembler.cpp
@@ -59,6 +59,7 @@ void ArmV8Disassembler::handleADRP(ArmV8Instruction& instruction) {
 
 void ArmV8Disassembler::handleBCond(ArmV8Instruction& instruction) {
     instruction.m_type = ArmV8InstructionType::B_Cond;
+    instruction.m_other = extractValue(0, 5, instruction.m_rawInstruction);
     instruction.m_immediate = extractValue(5, 19, instruction.m_rawInstruction) << 2;
     instruction.m_literal = m_baseAddress + (instruction.m_immediate);
 }
