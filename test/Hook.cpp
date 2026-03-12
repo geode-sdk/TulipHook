911f67cb86b980ee5c05ce9c2922b91abf65ccdf
Chloe <git@celosia.zip>
2026-03-11 21:25:36 -0700

    i think we have to get windows working

--- test/Hook.cpp
+++ test/Hook.cpp
@@ -217,30 +217,6 @@ TEST_F(HookTest, RecreateHandler) {
 	EXPECT_EQ(callFunction<10>(), 1);
 }
 
-int checkParams(int a, int b, int c, int d, int e, int f, int g, float h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s) {
-	EXPECT_EQ(a, 1);
-	EXPECT_EQ(b, 2);
-	EXPECT_EQ(c, 3);
-	EXPECT_EQ(d, 4);
-	EXPECT_EQ(e, 5);
-	EXPECT_EQ(f, 6);
-	EXPECT_EQ(g, 7);
-	EXPECT_EQ(h, 8.0f);
-	EXPECT_EQ(i, 9);
-	EXPECT_EQ(j, 10);
-	EXPECT_EQ(k, 11);
-	EXPECT_EQ(l, 12);
-	EXPECT_EQ(m, 13);
-	EXPECT_EQ(n, 14);
-	EXPECT_EQ(o, 15);
-	EXPECT_EQ(p, 16);
-	EXPECT_EQ(q, 17);
-	EXPECT_EQ(r, 18);
-	EXPECT_EQ(s, 19);
-
-	return 11;
-}
-
 int checkParamsHook(int a, int b, int c, int d, int e, int f, int g, float h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s) {
 	EXPECT_EQ(a, 1);
 	EXPECT_EQ(b, 2);
@@ -280,53 +256,6 @@ TEST_F(HookTest, SingleHookCheckParams) {
 
 	// hook->original
 	EXPECT_EQ(checkParams(1, 2, 3, 4, 5, 6, 7, 8.0f, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19), 12);
-}
-
-struct CheckParamsStruct {
-	int a;
-	int b;
-	int c;
-	int d;
-	double e;
-	int f;
-	int g;
-	float h;
-	int i;
-	int j;
-	int k;
-	int l;
-	std::int64_t m;
-	int n;
-	int o;
-	int p;
-	int q;
-	int r;
-	int s;
-};
-
-int checkStructParams(int a, CheckParamsStruct s) {
-	EXPECT_EQ(a, -1);
-	EXPECT_EQ(s.a, 1);
-	EXPECT_EQ(s.b, 2);
-	EXPECT_EQ(s.c, 3);
-	EXPECT_EQ(s.d, 4);
-	EXPECT_EQ(s.e, 123456789.1234);
-	EXPECT_EQ(s.f, 6);
-	EXPECT_EQ(s.g, 7);
-	EXPECT_EQ(s.h, 8.0f);
-	EXPECT_EQ(s.i, 9);
-	EXPECT_EQ(s.j, 10);
-	EXPECT_EQ(s.k, 11);
-	EXPECT_EQ(s.l, 12);
-	EXPECT_EQ(s.m, 123456789123456789ll);
-	EXPECT_EQ(s.n, 14);
-	EXPECT_EQ(s.o, 15);
-	EXPECT_EQ(s.p, 16);
-	EXPECT_EQ(s.q, 17);
-	EXPECT_EQ(s.r, 18);
-	EXPECT_EQ(s.s, 19);
-
-	return 4;
 }
 
 int checkStructParamsHook(int a, CheckParamsStruct s) {
--- test/hooksdef/HooksDef.hpp
+++ test/hooksdef/HooksDef.hpp
@@ -1,12 +1,44 @@
 #include <cstdint>
 
+#ifdef _WIN32
+	#define DEFINITION_EXPORT __declspec(dllexport)
+#else
+	#define DEFINITION_EXPORT
+#endif
+
+struct CheckParamsStruct {
+	int a;
+	int b;
+	int c;
+	int d;
+	double e;
+	int f;
+	int g;
+	float h;
+	int i;
+	int j;
+	int k;
+	int l;
+	std::int64_t m;
+	int n;
+	int o;
+	int p;
+	int q;
+	int r;
+	int s;
+};
+
+DEFINITION_EXPORT int checkParams(int a, int b, int c, int d, int e, int f, int g, float h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s);
+
+DEFINITION_EXPORT int checkStructParams(int a, CheckParamsStruct s);
+
 // should be passed through X0-X1 on ARM64
 struct CheckSmallStruct128 {
 	std::uint64_t x;
 	std::uint64_t y;
 };
 
-CheckSmallStruct128 checkSmallStruct128(CheckSmallStruct128 s);
+DEFINITION_EXPORT CheckSmallStruct128 checkSmallStruct128(CheckSmallStruct128 s);
 
 // should be passed either in one register (ARM64) or two (ARMv7)
 struct CheckSmallStruct64 {
@@ -14,4 +46,4 @@ struct CheckSmallStruct64 {
 	std::uint32_t b;
 };
 
-CheckSmallStruct64 checkSmallStruct64(CheckSmallStruct64 s);
+DEFINITION_EXPORT CheckSmallStruct64 checkSmallStruct64(CheckSmallStruct64 s);
